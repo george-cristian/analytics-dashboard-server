f908b2488a6369a725d5df0511e1f2330451a4fc
George Cristian <george.crist5@gmail.com>
2023-04-15 23:00:41 +0200

    Added user registration and login using knox

---
+++ users/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        """
+        Function which creates a new user in the database.
+        """
+        username = validated_data.get('username')
+        password = validated_data.get('password')
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        return user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
 
-# Create your views here.
+from .serializers import RegisterSerializer, UserSerializer
+
+
+class RegisterAPI(generics.GenericAPIView):
+    """
+    View for the register API.
+    """
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        """
+        Post function to register a new user.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        _, token = AuthToken.objects.create(user)
+
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": token
+        })
+
+
+class LoginAPI(KnoxLoginView):
+    """
+    View for the loging API using knox to manage authentication tokens.
+    """
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        """
+        Post function to login a user.
+        """
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data['user']
+        login(request, user)
+
+        return super(LoginAPI, self).post(request, format=None)
